d584678077b6e4cf8931bfd31c6e01d56eb6a66d
Codingwithvm <victormmendes.dev@gmail.com>
2024-04-30 18:33:06 -0300

    LoginScreen - Primeiro Sistema de Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react"
 import LoginScreen from "./components/LoginScreen/LoginScreen"
 
 const App = () => {
+    const [isLogged, setIsLogged] = useState(false)
+
+    const handleLogin = () => {
+        setIsLogged(true)
+    }
+
     return (
         <main>
-            <LoginScreen />
+            {!isLogged ? (
+                <LoginScreen onLogin={handleLogin} />
+            ) : (
+                // Aqui você pode renderizar outros componentes da sua aplicação após o login
+                <div>
+                    <h1>Bem-vindo ao WebDesk!</h1>
+                    {/* Outros componentes da sua aplicação */}
+                </div>
+            )}
         </main>
     )
 }
--- src/components/LoginScreen/LoginScreen.jsx
+++ src/components/LoginScreen/LoginScreen.jsx
@@ -1,14 +1,25 @@
+import { useState } from 'react'
 import './style.css'
 
-export default () => {
+export default ({ onLogin }) => {
+    const handleFormSubmit = (event) => {
+        event.preventDefault()
+
+        const username = event.target.elements.username.value 
+        const password = event.target.elements.password.value 
+
+        if (username === "root" && password === "root") onLogin()
+        else console.log('Credenciais Incorretas')
+    }
+
     return (
         <div className="desktop">
             <h1 className="logo">
                 WebDesk
             </h1>
-            <form>
-                <input type="text" placeholder="Username" required />
-                <input type="password" placeholder="Password" required />
+            <form onSubmit={handleFormSubmit}>
+                <input type="text" id="username" placeholder="Username" required />
+                <input type="password" id="password" placeholder="Password" required />
                 <input type="submit" value=">" />
             </form>
         </div>
