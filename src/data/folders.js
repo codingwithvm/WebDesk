4eb47e9332a13b9599fdd1f5034d9acc4cc1a1c7
Codingwithvm <victormmendes.dev@gmail.com>
2024-05-02 16:55:34 -0300

    Data - Adição do Objetos de Pastas

---
+++ src/data/folders.js
@@ -0,0 +1,38 @@
+const folders = [
+    {
+        id: 1,
+        name: "Meu Computador",
+        files: [],
+        subfolders: []
+    },
+    {
+        id: 2,
+        name: "Lixeira",
+        files: [],
+        subfolders: []
+    },
+    {
+        id: 3,
+        name: "Área de Trabalho",
+        files: [],
+        subfolders: []
+    },
+    {
+        id: 4,
+        name: "Downloads",
+        files: [],
+        subfolders: []
+    },
+    {
+        id: 5,
+        name: "Documentos",
+        files: [],
+        subfolders: []
+    },
+    {
+        id: 6,
+        name: "Imagens",
+        files: [],
+        subfolders: []
+    }
+]
